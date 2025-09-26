c44a83a25088c687c1744865e3103a23ee668706
Jorge Luis <jorge.luz@ufpi.edu.br>
2025-09-26 14:58:32 -0300

    feat: extrato e relatorio de extraro feitos

--- Frontend/wwwroot/js/main.js
+++ Frontend/wwwroot/js/main.js
@@ -6,12 +6,12 @@
 // Mude esta versão sempre que atualizar o sistema (ex: '1.0.1')
 const APP_VERSION = '1.0.0';
 
-const API_BASE_URL = 'https://api.ceramicacanelas.shop/api';
-// const API_BASE_URL = 'http://localhost:5087/api';
+// const API_BASE_URL = 'https://api.ceramicacanelas.shop/api';
+const API_BASE_URL = 'http://localhost:5087/api';
 
 // Cache para a tabela de histórico em páginas complexas
 let historyItemsCache = []; 
-
+const originalRowHTML_Extract = {};
 // Objetos para guardar o estado original da linha durante a edição
 const originalRowHTML_Product = {};
 const originalRowHTML_Employee = {};
