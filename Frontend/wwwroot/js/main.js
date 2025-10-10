e2e8d87127eb4463b580f94c8351ac6dfc70e4af
Jorge Luis <jorge.luz@ufpi.edu.br>
2025-10-09 22:56:33 -0300

    atss

--- Frontend/wwwroot/js/main.js
+++ Frontend/wwwroot/js/main.js
@@ -6,8 +6,8 @@
 // Mude esta versão sempre que atualizar o sistema (ex: '1.0.1')
 const APP_VERSION = '1.0.0';
 
-// const API_BASE_URL = 'https://api.ceramicacanelas.shop/api';
-const API_BASE_URL = 'http://localhost:5087/api';
+const API_BASE_URL = 'https://api.ceramicacanelas.shop/api';
+// const API_BASE_URL = 'http://localhost:5087/api';
 const originalRowHTML = {};
 // =======================================================
 let categoriesCache = [];
